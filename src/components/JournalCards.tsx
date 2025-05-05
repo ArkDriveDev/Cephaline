e401f4d7fa072a9de5391313e877bd45637bc6c8
JethroBalaga <balagajet@gmail.com>
2025-05-05 19:46:10 +0800

    added JournalCards dynamic element on creating card, duplicate new button

--- src/components/JournalCards.tsx
+++ src/components/JournalCards.tsx
@@ -1,19 +1,70 @@
-import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonPopover, IonContent} from '@ionic/react';
+import React, { useState } from 'react';
+import { IonButton, IonIcon, IonModal, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonInput, IonTextarea,IonItem,IonLabel} from '@ionic/react';
+import { bookOutline, close } from 'ionicons/icons';
 
-function JournalCards() {
+function NewButton() {
+  const [showModal, setShowModal] = useState(false);
+  const [journalTitle, setJournalTitle] = useState('');
+  const [journalDescription, setJournalDescription] = useState('');
+
+  const handleSave = () => {
+    // Here you would typically save to state or backend
+    console.log('New Journal:', { journalTitle, journalDescription });
+    setShowModal(false);
+    // Reset fields
+    setJournalTitle('');
+    setJournalDescription('');
+  };
+
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle id="hover-trigger" color='secondary'>Programming 1</IonCardTitle>
-            <IonPopover trigger="hover-trigger" triggerAction="hover">
-        <IonContent class="ion-padding">Open Journal</IonContent>
-</IonPopover>
-        <IonCardSubtitle>Fundamentals of Programming</IonCardSubtitle>
-      </IonCardHeader>
+    <>
+      <IonButton onClick={() => setShowModal(true)}>
+        <IonIcon slot="start" icon={bookOutline}></IonIcon>
+        New
+      </IonButton>
 
-      <IonCardContent>Created at /07/23/2025</IonCardContent>
-    </IonCard>
+      <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonButton onClick={() => setShowModal(false)}>
+                <IonIcon icon={close} />
+              </IonButton>
+            </IonButtons>
+            <IonTitle>Create New Journal</IonTitle>
+            <IonButtons slot="end">
+              <IonButton strong={true} onClick={handleSave}>
+                Save
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+            <IonItem>
+              <IonLabel position="stacked">Journal Title</IonLabel>
+              <IonInput
+                value={journalTitle}
+                onIonChange={(e) => setJournalTitle(e.detail.value || '')}
+                placeholder="Enter journal title"
+              />
+            </IonItem>
+            
+            <IonItem>
+              <IonLabel position="stacked">Description</IonLabel>
+              <IonTextarea
+                value={journalDescription}
+                onIonChange={(e) => setJournalDescription(e.detail.value || '')}
+                placeholder="Enter journal description"
+                rows={3}
+                autoGrow
+              />
+            </IonItem>
+          </div>
+        </IonContent>
+      </IonModal>
+    </>
   );
 }
-export default JournalCards;
+
+export default NewButton;
