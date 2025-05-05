d0a75c6291ef110c05d51eb075516cf9ed99c789
Arky <20231736@nbsc.edu.ph>
2025-05-05 08:33:07 +0800

    added searchbar, new buton and card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Profile from '../components/Profile';
-
+import Searchbar from '../components/Searchbar';
+import NewButton from '../components/NewButton';
+import JournalCards from '../components/JournalCards';
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Journal</IonTitle>
+          <IonTitle>Journals</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <Profile/>
+    <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+  <Searchbar />
+  <NewButton />
+</div>
+
+<JournalCards/>
+
       </IonContent>
     </IonPage>
   );
