77b5853158da0ea9308f4785c3887a0bc7eb34da
JethroBalaga <balagajet@gmail.com>
2025-05-05 19:54:52 +0800

    updated Newbutton.tsx to interact with journalCards.tsx, has a bug

--- src/components/NewButton.tsx
+++ src/components/NewButton.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
-import { IonButton, IonIcon, IonModal, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonInput, IonTextarea,IonItem,IonLabel} from '@ionic/react';
+import { IonButton, IonIcon, IonModal, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonInput, IonTextarea } from '@ionic/react';
 import { bookOutline, close } from 'ionicons/icons';
+import { NewJournalData } from '../types';
 
-function NewButton() {
+interface NewButtonProps {
+  onSave: (journal: NewJournalData) => void;
+}
+
+const NewButton: React.FC<NewButtonProps> = ({ onSave }) => {
   const [showModal, setShowModal] = useState(false);
   const [journalTitle, setJournalTitle] = useState('');
   const [journalDescription, setJournalDescription] = useState('');
 
   const handleSave = () => {
-    // Here you would typically save to state or backend
-    console.log('New Journal:', { journalTitle, journalDescription });
+    onSave({
+      title: journalTitle,
+      description: journalDescription
+    });
     setShowModal(false);
-    // Reset fields
     setJournalTitle('');
     setJournalDescription('');
   };
@@ -20,51 +26,14 @@ function NewButton() {
     <>
       <IonButton onClick={() => setShowModal(true)}>
         <IonIcon slot="start" icon={bookOutline}></IonIcon>
-        New
+        New Journal
       </IonButton>
 
       <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonButton onClick={() => setShowModal(false)}>
-                <IonIcon icon={close} />
-              </IonButton>
-            </IonButtons>
-            <IonTitle>Create New Journal</IonTitle>
-            <IonButtons slot="end">
-              <IonButton strong={true} onClick={handleSave}>
-                Save
-              </IonButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="ion-padding">
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-            <IonItem>
-              <IonLabel position="stacked">Journal Title</IonLabel>
-              <IonInput
-                value={journalTitle}
-                onIonChange={(e) => setJournalTitle(e.detail.value || '')}
-                placeholder="Enter journal title"
-              />
-            </IonItem>
-            
-            <IonItem>
-              <IonLabel position="stacked">Description</IonLabel>
-              <IonTextarea
-                value={journalDescription}
-                onIonChange={(e) => setJournalDescription(e.detail.value || '')}
-                placeholder="Enter journal description"
-                rows={3}
-                autoGrow
-              />
-            </IonItem>
-          </div>
-        </IonContent>
+        {/* ... rest of the modal code ... */}
       </IonModal>
     </>
   );
-}
+};
 
 export default NewButton;
